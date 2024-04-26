23e8cf3ab6e976cd78ba2c600b54ede2906e85c8
SaiEswar15 <saieswarkumar@gmail.com>
2024-04-26 21:54:12 +0530

    run only one test case with 'test.only(, ()=>{})'

--- src/components/greet/Greeting.test.tsx
+++ src/components/greet/Greeting.test.tsx
@@ -9,7 +9,7 @@ test("renders text correctly", ()=>{
 
 //writing test using the TDD method 
 //in this first we write the test and get errors and then solve them to be green.
-test("renders with name", ()=>{
+test.only("renders with name", ()=>{
     render(<Greeting name = "Eswar"/>)
     const textWithNameElement = screen.getByText("Hello Eswar");
     expect(textWithNameElement).toBeInTheDocument();
