de10444ae5971bb47ce0c820fb55932276739df8
SaiEswar15 <saieswarkumar@gmail.com>
2024-04-26 22:18:57 +0530

    file extension .spec

---
+++ src/components/greet/Greeting.spec.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react"
+import Greeting from "./Greeting";
+
+
+describe("specs", ()=>{
+
+    test("renders text", ()=>{
+        render(<Greeting/>);
+        const textElement = screen.getByText("Hello");
+        expect(textElement).toBeInTheDocument();
+    })
+
+    //writing test using the TDD method 
+    //in this first we write the test and get errors and then solve them to be green.
+    test("renders with name", ()=>{
+        render(<Greeting name = "Eswar"/>)
+        const textWithNameElement = screen.getByText("Hello Eswar");
+        expect(textWithNameElement).toBeInTheDocument();
+    })
+})
+
+
+
+
