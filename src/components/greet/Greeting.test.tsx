85d3d5c7d5f98517e80499b918cc036858f8f80b
SaiEswar15 <saieswarkumar@gmail.com>
2024-04-26 22:00:08 +0530

    grouping with describe

--- src/components/greet/Greeting.test.tsx
+++ src/components/greet/Greeting.test.tsx
@@ -1,16 +1,25 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting";
 
-test("renders text correctly", ()=>{
-    render(<Greeting/>);
-    const textElement = screen.getByText("Hello");
-    expect(textElement).toBeInTheDocument();
+
+describe("greet", ()=>{
+
+    test("renders text", ()=>{
+        render(<Greeting/>);
+        const textElement = screen.getByText("Hello");
+        expect(textElement).toBeInTheDocument();
+    })
 })
 
-//writing test using the TDD method 
-//in this first we write the test and get errors and then solve them to be green.
-test.only("renders with name", ()=>{
-    render(<Greeting name = "Eswar"/>)
-    const textWithNameElement = screen.getByText("Hello Eswar");
-    expect(textWithNameElement).toBeInTheDocument();
+describe("group", ()=>{
+
+    //writing test using the TDD method 
+    //in this first we write the test and get errors and then solve them to be green.
+    test("renders with name", ()=>{
+        render(<Greeting name = "Eswar"/>)
+        const textWithNameElement = screen.getByText("Hello Eswar");
+        expect(textWithNameElement).toBeInTheDocument();
+    })
 })
+
+
